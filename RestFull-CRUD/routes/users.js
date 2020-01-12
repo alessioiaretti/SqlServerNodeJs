9d09d0418efdf7cc91fb1dfe7e8516611d6e2ecd
alessioiaretti <iaretti.alessio@iisgalvanimi.edu.it>
2020-01-12 23:22:17 +0000

    1

--- RestFull-CRUD/routes/users.js
+++ RestFull-CRUD/routes/users.js
@@ -10,17 +10,31 @@ const config = {
 }
 
 /* GET users listing. */
-router.get('/attori', function(req, res, next) {
+router.get('/prodotti', function(req, res, next) {
   sql.connect(config, err => {
     if(err) console.log(err);  // ... error check
     
     // Query
     let sqlRequest = new sql.Request();  //Oggetto che serve a creare le query
-    sqlRequest.query('select * from dbo.AA_ATTORE', (err, result) => {
+    sqlRequest.query('select * from dbo.[cr-unit-attributes]', (err, result) => {
         if (err) console.log(err); // ... error checks
         res.send(result);  //Invio il risultato
     });
   });
 });
 
+router.get('/search/:name', function(req, res, next) {
+  sql.connect(config, err => {
+    // ... error check
+    if(err) console.log(err);
+    // Query
+    let sqlRequest = new sql.Request();
+    sqlRequest.query(`select * from dbo.[cr-unit-attributes] where Unit = '${req.params.name}'`, (err, result) => {
+        // ... error checks
+        if (err) console.log(err);
+
+        res.send(result);
+    });
+  });
+});
 module.exports = router;
