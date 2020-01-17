9fe900e923cc44dd16c0270f381437ba5c68ca2f
alessioiaretti <iaretti.alessio@iisgalvanimi.edu.it>
2020-01-17 00:02:11 +0000

    1

--- RestFull-CRUD/routes/index.js
+++ RestFull-CRUD/routes/index.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET insert page. */
+router.get('/insert', function(req, res, next) {
+  res.render('insert', { title: 'Express' });
+});
+
+/* GET list page. */
+router.get('/elenco', function(req, res, next) {
+  res.render('elenco', { title: 'Express' });
+});
+
+
 module.exports = router;
