c2569fb4875f30a89949b19cc710b0fc44a61377
alessioiaretti <iaretti.alessio@iisgalvanimi.edu.it>
2020-03-30 21:35:07 +0000

    2

--- AngRestFull-CRUD/src/app/app.component.ts
+++ AngRestFull-CRUD/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Unit } from './unit.model';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngRestFull-CRUD';
+  obsUnit: Observable<Unit[]>; //L’observable che sta in attesa dei dati
+  data: Unit[];
+  constructor(private http: HttpClient) { } //Dependency injection
+  getUnitList(): void {
+    //Qui va sostituito l’url con quello delle vostre api
+    this.obsUnit = this.http.get<Unit[]>('https://3000-e4dd8951-c918-46c9-b98e-5c8a043256bb.ws-eu01.gitpod.io/users');
+    //Mi sottoscrivo all’observable e scrivo la arrow function che riceve i dati
+    this.obsUnit.subscribe((data: Unit[]) => {this.data = data;});
+  };
+
+  postObserver : Observable<Object>;
+  postData : Object;
+
+  addUnit (newUnit: HTMLInputElement, newCost: HTMLInputElement, newHitSpeed: HTMLInputElement): boolean {
+      let newData: Unit = new Unit();
+      newData.Unit = newUnit.value;
+      newData.Cost = newCost.value;
+      newData.Hit_Speed = newHitSpeed.value;
+      let headers =  {headers: new HttpHeaders().set('Content-Type', 'application/json')};
+      this.postObserver = this.http.post('https://3000-e4dd8951-c918-46c9-b98e-5c8a043256bb.ws-eu01.gitpod.io/users', JSON.stringify(newData),headers)
+
+      //Meglio così ma da verificare
+      //this.postObserver = this.http.post('http://localhost:3000/users', newData);
+      this.postObserver.subscribe(data => this.postData = data);
+      return false;
+  }
 }
+
+
--- AngRestFull-CRUD/src/app/app.module.ts
+++ AngRestFull-CRUD/src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http'; //Importa Il modulo http
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HttpClientModule //Aggiungi il modulo http qui
   ],
   providers: [],
   bootstrap: [AppComponent]
